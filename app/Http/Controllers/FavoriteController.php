0286b4279a99c585f19ab90925a71864f9e63dce
Fayoz-dev <frasulov988@gmail.com>
2024-01-27 12:50:29 +0500

    favorites

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use http\Env\Response;
+use Illuminate\Http\Request;
+
+class FavoriteController extends Controller
+{
+    public function __construct()
+    {
+       $this->middleware('auth:sanctum');
+    }
+
+    public function index()
+    {
+        return auth()->user()->favorites()->paginate(20);
+    }
+
+    public function store(Request $request)
+    {
+         auth()->user()->favorites()->attach($request->product_id);
+
+         return response()->json([
+            'success' => true,
+            'message' => 'add favorites success'
+         ]);
+    }
+
+    public function destroy($favorite_id)
+    {
+       if (auth()->user()->hasFavorite($favorite_id)){
+           auth()->user()->favorites()->detach($favorite_id);
+
+           return response()->json(['success' => true, 'message' => 'Deleted success !']);
+       }
+
+       return response()->json(['success' => false, 'message' => 'No favorite']);
+    }
+}
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,4 +25,9 @@ class Product extends Model
     {
         return $this->hasMany(Stock::class);
     }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
